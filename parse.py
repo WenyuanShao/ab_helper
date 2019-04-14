2b92bd22f34028d48045e3f6cbf5591c864f6833
graceliu <graceliu@node-1.6-nodes-c220g1.gwcloudlab-pg0.wisc.cloudlab.us>
2019-04-13 20:09:02 -0500

    add the counter of the parse.py

--- parse.py
+++ parse.py
@@ -3,6 +3,7 @@ import os
 path = "logs/"
 files = os.listdir(path)
 s = []
+num = 0
 total_result = 0
 request_per_sec = 0
 
@@ -13,7 +14,9 @@ for file in files:
                 print line
                 request_per_sec = line.split()[3].strip();
                 print request_per_sec
+		num += 1
                 break
         total_result += float(request_per_sec)
     
 print total_result
+print num
